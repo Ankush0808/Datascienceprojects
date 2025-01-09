a5b242f459573e4bb3ca61a8b39d1c00ba10d180
unknown <ankushtonde8@gmail.com>
2025-01-09 14:40:02 -0500

    Predicted win percentage for the teams

--- matchpredictor.py
+++ matchpredictor.py
@@ -2,6 +2,8 @@ import plotly as pt
 import numpy as np
 import pickle
 import streamlit as st
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 st.title("Welcome to IPL Match predictions")
 a=st.selectbox("Select the batting team",['Royal Challengers Bangalore', 'Kings XI Punjab',
        'Delhi Daredevils', 'Kolkata Knight Riders', 'Rajasthan Royals',
@@ -21,7 +23,7 @@ b=st.selectbox("Select the bowling team",['Royal Challengers Bangalore', 'Kings 
        'Royal Challengers Bengaluru'],index=0)
 c= st.slider("select the over", min_value=0,max_value=19)
 d=st.slider("select the ball",min_value=1,max_value=11)
-e=st.selectbox("How many wickets down?",[0,1,2,3,4,5,6,7,8,9,10])
+e=st.selectbox("How many wickets down?",[0,1,2,3,4,5,6,7,8,9])
 f= st.number_input("whats the score?")
 g= st.number_input("whats the target?")
 h=st.number_input("Overs to chase that target?",min_value=1,max_value=20)
@@ -64,10 +66,14 @@ i= st.selectbox("Where is the match?",['M Chinnaswamy Stadium',
        'Himachal Pradesh Cricket Association Stadium, Dharamsala',
        'Maharaja Yadavindra Singh International Cricket Stadium, Mullanpur',
        'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium, Visakhapatnam'])
+input_data=[[a,b,c,d,e,f,g,h,i]]
 with open("C://Users//Lenovo//pipe_model.pkl", 'rb') as f:
-    pipe = pickle.load(f)
+     pipe = pickle.load(f)
 
 if st.button("Click here to predict"):
-    input_data=np.array([[a,b,c,d,e,f,g,h,i]])
-    prediction = pipe.predict(input_data)
-    st.success(prediction)
+    prediction = pipe.predict_proba(input_data)
+    defending_prob = prediction[0][0] * 100  # Probability for the first team
+    Chasing_prob = prediction[0][1] * 100 
+    st.write(f"Win percentage for {a} would be {Chasing_prob:.2f}%")
+    st.write(f"Win percentage for {b} would be {defending_prob:.2f}%")
+    
